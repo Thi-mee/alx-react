8c7783d3770e018d9e85bc5a8f8d22e0761b77b5
Timi Adenuga <85906514+Thi-mee@users.noreply.github.com>
2023-04-18 04:37:15 +0000

    tasks done

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, "js/dashboard_main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+}
